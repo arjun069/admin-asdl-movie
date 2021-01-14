37cef59c7f5925e2881b429349966b3acf2f71db
Arjun <arjunnair2k18@gmail.com>
2021-01-14 22:01:38 +0530

    admin done

---
+++ lib/home.dart
@@ -0,0 +1,93 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  Home({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final dbRef = FirebaseDatabase.instance.reference().child("movies");
+  final dbRefupcoming =
+      FirebaseDatabase.instance.reference().child("upcomingmovies");
+  List<Map<dynamic, dynamic>> lists = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              FutureBuilder(
+                  future: dbRef.once(),
+                  builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
+                    if (snapshot.hasData) {
+                      lists.clear();
+                      Map<dynamic, dynamic> values = snapshot.data.value;
+                      values.forEach((key, values) {
+                        lists.add(values);
+                      });
+                      return new ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: lists.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            if (lists[index]["thisorthat"] == "Current") {
+                              return Card(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text("Name: " + lists[index]["name"]),
+                                    Image.network(lists[index]["url"]),
+                                    Text("Age: " + lists[index]["url"]),
+                                    Text("Type: " + lists[index]["type"]),
+                                    Text("thisorthat : " +
+                                        lists[index]["thisorthat"])
+                                  ],
+                                ),
+                              );
+                            }
+                          });
+                    }
+                    return CircularProgressIndicator();
+                  }),
+              FutureBuilder(
+                  future: dbRefupcoming.once(),
+                  builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
+                    if (snapshot.hasData) {
+                      lists.clear();
+                      Map<dynamic, dynamic> values = snapshot.data.value;
+                      values.forEach((key, values) {
+                        lists.add(values);
+                      });
+                      return new ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: lists.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Card(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text("Name: " + lists[index]["name"]),
+                                  Image.network(lists[index]["url"]),
+                                  Text("Age: " + lists[index]["url"]),
+                                  Text("Type: " + lists[index]["type"]),
+                                  Text("thisorthat : " +
+                                      lists[index]["thisorthat"])
+                                ],
+                              ),
+                            );
+                          });
+                    }
+                    return CircularProgressIndicator();
+                  })
+            ],
+          ),
+        ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,252 @@
+import 'package:asdl/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Admin panel for adding movies',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Register Pet'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+          child: SingleChildScrollView(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Please Add movies ",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w200,
+                      fontSize: 30,
+                      fontFamily: 'Roboto',
+                      fontStyle: FontStyle.italic)),
+              RegisterPet(),
+            ]),
+      )),
+    );
+  }
+}
+
+class RegisterPet extends StatefulWidget {
+  RegisterPet({Key key}) : super(key: key);
+
+  @override
+  _RegisterPetState createState() => _RegisterPetState();
+}
+
+class _RegisterPetState extends State<RegisterPet> {
+  final _formKey = GlobalKey<FormState>();
+  final listOfMovies = ["English", "Hindi", "Malayalam"];
+  String dropdownValue = 'English';
+  final thisorthat = ["Current", "Upcoming"];
+  String dropdownValuethis = 'Current';
+  final nameController = TextEditingController();
+  final urlController = TextEditingController();
+  final dbRef = FirebaseDatabase.instance.reference().child("movies");
+  final dbRefupcoming =
+      FirebaseDatabase.instance.reference().child("upcomingmovies");
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+            child: Column(children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: TextFormField(
+              controller: nameController,
+              decoration: InputDecoration(
+                labelText: "Enter Movie Name",
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              // The validator receives the text that the user has entered.
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Enter Movie Name';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: DropdownButtonFormField(
+              value: dropdownValue,
+              icon: Icon(Icons.arrow_downward),
+              decoration: InputDecoration(
+                labelText: "Select Movie Language",
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              items: listOfMovies.map((String value) {
+                return new DropdownMenuItem<String>(
+                  value: value,
+                  child: new Text(value),
+                );
+              }).toList(),
+              onChanged: (String newValue) {
+                setState(() {
+                  dropdownValue = newValue;
+                });
+              },
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please select movie language';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: TextFormField(
+              keyboardType: TextInputType.url,
+              controller: urlController,
+              decoration: InputDecoration(
+                labelText: "Enter Image url for picture",
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              // The validator receives the text that the user has entered.
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter url';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: DropdownButtonFormField(
+              value: dropdownValuethis,
+              icon: Icon(Icons.arrow_downward),
+              decoration: InputDecoration(
+                labelText: "Select In theatres or upcoming",
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              items: thisorthat.map((String value) {
+                return new DropdownMenuItem<String>(
+                  value: value,
+                  child: new Text(value),
+                );
+              }).toList(),
+              onChanged: (String newValue) {
+                setState(() {
+                  dropdownValuethis = newValue;
+                });
+              },
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please select if its currently running or upcoming';
+                }
+                return null;
+              },
+            ),
+          ),
+          Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  RaisedButton(
+                    color: Colors.lightBlue,
+                    onPressed: () {
+                      if (dropdownValuethis == "Current") {
+                        if (_formKey.currentState.validate()) {
+                          dbRef.push().set({
+                            "name": nameController.text,
+                            "url": urlController.text,
+                            "type": dropdownValue,
+                            "thisorthat": dropdownValuethis,
+                          }).then((_) {
+                            Scaffold.of(context).showSnackBar(
+                                SnackBar(content: Text('Successfully Added')));
+                            urlController.clear();
+                            nameController.clear();
+                          }).catchError((onError) {
+                            Scaffold.of(context)
+                                .showSnackBar(SnackBar(content: Text(onError)));
+                          });
+                        }
+                      } else {
+                        if (_formKey.currentState.validate()) {
+                          dbRefupcoming.push().set({
+                            "name": nameController.text,
+                            "url": urlController.text,
+                            "type": dropdownValue,
+                            "thisorthat": dropdownValuethis,
+                          }).then((_) {
+                            Scaffold.of(context).showSnackBar(
+                                SnackBar(content: Text('Successfully Added')));
+                            urlController.clear();
+                            nameController.clear();
+                          }).catchError((onError) {
+                            Scaffold.of(context)
+                                .showSnackBar(SnackBar(content: Text(onError)));
+                          });
+                        }
+                      }
+                    },
+                    child: Text('Submit'),
+                  ),
+                  RaisedButton(
+                    color: Colors.amber,
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => Home(title: "Home Page")),
+                      );
+                    },
+                    child: Text('Navigate'),
+                  ),
+                ],
+              )),
+        ])));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    urlController.dispose();
+    nameController.dispose();
+  }
+}
